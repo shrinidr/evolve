f991cf2d49a2ecbd6a71a81b843ff77b3af3ca2b
shrinidr <rushilk@bu.edu>
2026-04-17 17:21:49 -0400

    Initial work using LLVM IR for the lin alg engine

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Early stages of a C++ engine to solve important computational problems in biology.
+I am starting out by first trying to build a linear algebra engine w/ heavy LLVM IR
+optimizations in place.
+After that is done, and I have firmed grip over writing performant code, I will start working on CFD slowly, maybe partnering up with a physics major.
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,9 @@
-#include <vector>
-#include <cstdint>
+#include <iostream>
 
-struct SpatialGrid {
-    int width, height, cellSize;
-    int cols, rows;
 
-    std::vector<std::vector<uint32_t>> buckets;
-    // this is the constructor for our grid struct. The width(w) and height(h)
-    // are like setter functions.
-    
-    SpatialGrid(int w, int h, int size) : width(w), height(h), cellSize(size) {
-        cols = width / cellSize;
-        rows = height / cellSize;
-        buckets.resize(cols * rows);
-    }
-
-    int getIndex(float x, float y) {
-        int cx = static_cast<int>(x) / cellSize;
-        int cy = static_cast<int>(y) / cellSize;
-        cx = std::max(0, std::min(cx, cols - 1));
-        cy = std::max(0, std::min(cy, rows - 1));
-        return cx + cy * cols;
-    }
-
-    void addEntity(uint32_t id, float x, float y) {
-        buckets[getIndex(x, y)].push_back(id);
-    }
-    void clear() {
-        for (auto& bucket : buckets) bucket.clear();
-    }
-};
+int add(int one, int two){
+    return (one + two);
+}
+int main(){
+    return add(3, 4);
+}
